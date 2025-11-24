2a50716947a9579dde3de9b505cd2fc158d4da1b
Daywalkr <8580915+Daywalkrr@users.noreply.github.com>
2025-11-24 16:56:48 -0500

    tidy up processing logic

---
+++ Intersect.Server/MapInstance.NpcWaves.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Intersect.Server.Maps
+{
+    public partial class MapInstance : IDisposable
+    {
+        protected NpcWaveController NpcWaveController { get; set; }
+
+        protected void InitializeNpcWaves()
+        {
+            NpcWaveController = new NpcWaveController(this);
+            NpcWaveController.Initialize();
+        }
+
+        protected void ProcessNpcWaves()
+        {
+            NpcWaveController.ProcessWaves();
+        }
+    }
+}
