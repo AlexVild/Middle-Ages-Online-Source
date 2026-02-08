0016e47edf76c16995ac7dce0a6b34dd87868a3c
Daywalkr <8580915+Daywalkrr@users.noreply.github.com>
2026-02-08 16:48:59 -0500

    feat: (Day) Add logic to learn any missing challenge spells; add logic to change cursor in combat mode:

--- Intersect.Server/Entities/Player.cs
+++ Intersect.Server/Entities/Player.cs
@@ -485,6 +485,7 @@ namespace Intersect.Server.Entities
 
             ReprocessEnhancements();
             ValidateCurrentWeaponLevels();
+            ValidateChallengeSpellUnlocks();
             PermaHeld = false;
         }
 
